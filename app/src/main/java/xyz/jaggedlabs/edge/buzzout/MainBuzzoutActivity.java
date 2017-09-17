17017c0d9a1f4373fbade6e17f5b612fdd823595
Gustavo Gomes <gustavo.gomes@timwe.com>
2017-09-17 12:21:00 +0100

    Minor rename of the main Activity, which was just Buzzout and the new name explicitly tells the developers it is the main activity.

--- app/src/main/java/xyz/jaggedlabs/edge/buzzout/MainBuzzoutActivity.java
+++ app/src/main/java/xyz/jaggedlabs/edge/buzzout/MainBuzzoutActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class Buzzout extends AppCompatActivity
+public class MainBuzzoutActivity extends AppCompatActivity
 {
 
     private TextView mTextMessage;
