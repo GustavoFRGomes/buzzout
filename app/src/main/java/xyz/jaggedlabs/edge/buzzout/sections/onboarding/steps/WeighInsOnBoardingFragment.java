643d16afca9c04bbb4b57aa0b41ace5577dc8058
Gustavo Gomes <gustavo.gomes@timwe.com>
2017-09-24 07:28:08 +0100

    Almost finalized the WeighIns logic, of course there is still missing the persistence logic.

--- app/src/main/java/xyz/jaggedlabs/edge/buzzout/sections/onboarding/steps/WeighInsOnBoardingFragment.java
+++ app/src/main/java/xyz/jaggedlabs/edge/buzzout/sections/onboarding/steps/WeighInsOnBoardingFragment.java
@@ -6,17 +6,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import xyz.jaggedlabs.edge.buzzout.R;
 import xyz.jaggedlabs.edge.buzzout.sections.onboarding.IOnOnboardingNavigationClicked;
 import xyz.jaggedlabs.edge.buzzout.sections.onboarding.OnBoardingSteps;
 
 /**
- * Created by gustavogomes on 17/09/2017.
+ * Gustavo Gomes on 17/09/2017.
  */
 
-public class WeighInsOnBoardingFragment extends Fragment implements View.OnClickListener
+public class WeighInsOnBoardingFragment extends Fragment implements View.OnClickListener, CompoundButton.OnCheckedChangeListener
 {
+    private enum WEIGHT_IN_INTERVALS { DAILY, WEEKLY, MONTHLY, X_DAYS }
+
     public static WeighInsOnBoardingFragment newInstance()
     {
         return new WeighInsOnBoardingFragment();
@@ -24,6 +30,14 @@ public class WeighInsOnBoardingFragment extends Fragment implements View.OnClick
 
     private Button nextPageButton;
     private Button previousPageButton;
+
+    private CheckBox daily;
+    private CheckBox weekly;
+    private CheckBox monthly;
+    private CheckBox xDays;
+
+    private EditText xDaysInput;
+    private TextView xDaysEmptyError;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceBundle)
@@ -36,6 +50,18 @@ public class WeighInsOnBoardingFragment extends Fragment implements View.OnClick
         this.previousPageButton = ((Button) rootView.findViewById(R.id.previous_button));
         this.previousPageButton.setOnClickListener(this);
 
+        this.daily = (CheckBox) rootView.findViewById(R.id.weight_in_daily);
+        this.weekly = (CheckBox) rootView.findViewById(R.id.weight_in_weekly);
+        this.monthly = (CheckBox) rootView.findViewById(R.id.weight_in_monthly);
+        this.xDays = (CheckBox) rootView.findViewById(R.id.weight_in_x_days);
+        this.xDaysInput = (EditText) rootView.findViewById(R.id.x_days_input);
+        this.xDaysEmptyError = (TextView) rootView.findViewById(R.id.empty_value_error);
+
+        this.daily.setOnCheckedChangeListener(this);
+        this.weekly.setOnCheckedChangeListener(this);
+        this.monthly.setOnCheckedChangeListener(this);
+        this.xDays.setOnCheckedChangeListener(this);
+
         return rootView;
     }
 
@@ -46,12 +72,69 @@ public class WeighInsOnBoardingFragment extends Fragment implements View.OnClick
         {
             if (v.getId() == this.nextPageButton.getId())
             {
-                ((IOnOnboardingNavigationClicked) this.getParentFragment()).nextPage(OnBoardingSteps.WEIGHINS_PAGE);
+                if (this.getSelectedInterval() == WEIGHT_IN_INTERVALS.X_DAYS && this.xDaysInput.getText().toString().isEmpty())
+                {
+                    this.xDaysEmptyError.setText(R.string.onboarding_weight_in_x_days_empty);
+                }
+                else
+                {
+                    this.xDaysEmptyError.setText("");
+                    ((IOnOnboardingNavigationClicked) this.getParentFragment()).nextPage(OnBoardingSteps.WEIGHINS_PAGE);
+                }
             }
             else if (v.getId() == this.previousPageButton.getId())
             {
                 ((IOnOnboardingNavigationClicked) this.getParentFragment()).previousPage(OnBoardingSteps.WEIGHINS_PAGE);
             }
         }
+    }
+
+    private WEIGHT_IN_INTERVALS getSelectedInterval() {
+        if (this.daily.isChecked())
+        {
+            return WEIGHT_IN_INTERVALS.DAILY;
+        }
+
+        if (this.weekly.isChecked())
+        {
+            return WEIGHT_IN_INTERVALS.WEEKLY;
+        }
+
+        if (this.monthly.isChecked())
+        {
+            return WEIGHT_IN_INTERVALS.MONTHLY;
+        }
+
+        return WEIGHT_IN_INTERVALS.X_DAYS;
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+    {
+        int id = buttonView.getId();
+
+        if (isChecked)
+        {
+            this.singleCheckAndDisableAction(
+                    this.daily.getId() == id,
+                    this.weekly.getId() == id,
+                    this.monthly.getId() == id,
+                    this.xDays.getId() == id);
+        }
+    }
+
+    private void singleCheckAndDisableAction(boolean daily, boolean weekly, boolean monthly, boolean xDays)
+    {
+        this.daily.setChecked(daily);
+        this.daily.setClickable(!daily);
+
+        this.monthly.setChecked(monthly);
+        this.monthly.setClickable(!monthly);
+
+        this.weekly.setChecked(weekly);
+        this.weekly.setClickable(!weekly);
+
+        this.xDays.setChecked(xDays);
+        this.xDays.setClickable(!xDays);
     }
 }
